bc46631a389b9ef6a9f106451b6ed1e37c921fdc
Satyam Gangwar <satyam.gangwar@highspring.in>
2026-02-09 16:35:05 +0530

    Add /new route to Flask app

--- deploy_app/main.py
+++ deploy_app/main.py
@@ -4,4 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Cloud Run! System check complete."
+    return "Hello from Satyam Gangwar from Cloud Run."
+
+@app.route("/analyze")
+def analyze():
+    return "Hello new route"
+
+if __name__ == "__main__":
+    print("Starting server...")
+    app.run(host="0.0.0.0", port=8080)
+
